93fab4a1369601be871ed01471e6371f02c7b493
angelalucban07-bit <angelalucban07@gmail.com>
2026-03-03 23:06:57 +0800

    initial commit

---
+++ Pomodoro_todo.py/pomotodo.py.py
@@ -0,0 +1,15 @@
+BOLD = '\033[1m'
+ITALIC = '\033[3m'
+END = '\033[0m'
+
+class ToDoList:
+    def __init__(self):
+        self.tasks = []
+
+    def addTask(self, task,
+        priority, due_date{
+            "task": task,
+            "done": False,
+        "priority": priority,
+        "due_date": due_date
+    }
